3a2635c32399a7c2221e213daf00bf3dec1b58f7
sammulvey747 <99451072+sammulvey747@users.noreply.github.com>
2023-01-21 23:06:25 -0500

    Revert "Revert "Using 364 BaseFalconSwerve as test""
    This reverts commit fe92b798d7df0661143be2b9bffe96bc71661343.

--- src/main/java/frc/robot/commands/SwerveCommands/SwerveDriveCommand.java
+++ src/main/java/frc/robot/commands/SwerveCommands/SwerveDriveCommand.java
@@ -1,88 +1,46 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.commands.SwerveCommands;
 
-import java.util.function.Supplier;
-
-import edu.wpi.first.math.filter.SlewRateLimiter;
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.math.kinematics.SwerveModuleState;
-import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.Constants.DriveConstants;
+import frc.robot.Constants;
 import frc.robot.subsystems.SwerveSubsystem;
 
-public class SwerveDriveCommand extends CommandBase {
-  /** Creates a new SwerveDriveCommand. */
+import java.util.function.BooleanSupplier;
+import java.util.function.DoubleSupplier;
 
-  private final SwerveSubsystem swerveSubsystem;
-  private final Supplier<Double> xSpdFunction, ySpdFunction, turningSpdFunction;
-  private final Supplier<Boolean> fieldOrientedFunction;
-  private final SlewRateLimiter xLimiter, yLimiter, turningLimiter;
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 
-  public SwerveDriveCommand(SwerveSubsystem swerve, Supplier<Double> xSpdFunction, Supplier<Double> ySpdFunction,
-    Supplier<Double> turningSpdFunction, Supplier<Boolean> fieldOrientedFunction) {
-    this.swerveSubsystem = swerve;
-    this.xSpdFunction = xSpdFunction;
-    this.ySpdFunction = ySpdFunction;
-    this.turningSpdFunction = turningSpdFunction;
-    this.fieldOrientedFunction = fieldOrientedFunction;
 
-    //slew rate limiter for smoother driving
-    this.xLimiter = new SlewRateLimiter(DriveConstants.kTeleDriveMaxAccelerationUnitsPerSecond);
-    this.yLimiter = new SlewRateLimiter(DriveConstants.kTeleDriveMaxAccelerationUnitsPerSecond);
-    this.turningLimiter = new SlewRateLimiter(DriveConstants.kTeleDriveMaxAngularAccelerationUnitsPerSecond);
+public class SwerveDriveCommand extends CommandBase {    
+    private SwerveSubsystem s_Swerve;    
+    private DoubleSupplier translationSup;
+    private DoubleSupplier strafeSup;
+    private DoubleSupplier rotationSup;
+    private BooleanSupplier robotCentricSup;
 
-    addRequirements(swerveSubsystem);
-  }
+    public SwerveDriveCommand(SwerveSubsystem s_Swerve, DoubleSupplier translationSup, DoubleSupplier strafeSup, DoubleSupplier rotationSup, BooleanSupplier robotCentricSup) {
+        this.s_Swerve = s_Swerve;
+        addRequirements(s_Swerve);
 
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {}
-
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-    //get the joystick values from the supplier
-    double xSpeed = xSpdFunction.get();
-    double ySpeed = ySpdFunction.get();
-    double turningSpeed = turningSpdFunction.get();
-
-    //deadband for controllers. Picked .03 from last year.
-    xSpeed = Math.abs(xSpeed) > .03 ? xSpeed : 0.0;
-    ySpeed = Math.abs(ySpeed) > .03 ? ySpeed : 0.0;
-    turningSpeed = Math.abs(turningSpeed) > .03 ? turningSpeed : 0.0;
-
-    //slew rates allow for smoother driving. 
-    //ensure the max speed is the physical max speed of the robot
-    xSpeed = xLimiter.calculate(xSpeed) * DriveConstants.kTeleDriveMaxAccelerationUnitsPerSecond;
-    ySpeed = yLimiter.calculate(ySpeed) * DriveConstants.kTeleDriveMaxAccelerationUnitsPerSecond;
-    turningSpeed = turningLimiter.calculate(turningSpeed) * DriveConstants.kTeleDriveMaxAngularAccelerationUnitsPerSecond;
-
-    //field oriented vs. robot centric driving
-    ChassisSpeeds chassisSpeeds;
-    if(fieldOrientedFunction.get()){
-      chassisSpeeds = ChassisSpeeds.fromFieldRelativeSpeeds(xSpeed, ySpeed, turningSpeed, swerveSubsystem.getRotation2d());
-    }
-    else{
-      chassisSpeeds = new ChassisSpeeds(xSpeed, ySpeed, turningSpeed);
+        this.translationSup = translationSup;
+        this.strafeSup = strafeSup;
+        this.rotationSup = rotationSup;
+        this.robotCentricSup = robotCentricSup;
     }
 
-    // Convert chassis speeds to individual module states
-    SwerveModuleState[] moduleStates = DriveConstants.kDriveKinematics.toSwerveModuleStates(chassisSpeeds);
-    swerveSubsystem.setModuleStates(moduleStates);
-  }
+    @Override
+    public void execute() {
+        /* Get Values, Deadband*/
+        double translationVal = MathUtil.applyDeadband(translationSup.getAsDouble(), Constants.stickDeadband);
+        double strafeVal = MathUtil.applyDeadband(strafeSup.getAsDouble(), Constants.stickDeadband);
+        double rotationVal = MathUtil.applyDeadband(rotationSup.getAsDouble(), Constants.stickDeadband);
 
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {
-    swerveSubsystem.stop();
-  }
-
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
-  }
+        /* Drive */
+        s_Swerve.drive(
+            new Translation2d(translationVal, strafeVal).times(Constants.Swerve.maxSpeed), 
+            rotationVal * Constants.Swerve.maxAngularVelocity, 
+            !robotCentricSup.getAsBoolean(), 
+            true
+        );
+    }
 }
