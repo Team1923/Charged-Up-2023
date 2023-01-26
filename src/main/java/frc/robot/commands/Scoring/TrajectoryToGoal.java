4341bba6de9b65c21f6e0a05c9f8508e94cb65b4
Aalind Tyagi <42896179+Squid5678@users.noreply.github.com>
2023-01-25 19:33:51 -0500

    B1.4.3AT - limelight trajectory, lots of testing to do

--- src/main/java/frc/robot/commands/Scoring/TrajectoryToGoal.java
+++ src/main/java/frc/robot/commands/Scoring/TrajectoryToGoal.java
@@ -1,67 +1,96 @@
-// // Copyright (c) FIRST and other WPILib contributors.
-// // Open Source Software; you can modify and/or share it under the terms of
-// // the WPILib BSD license file in the root directory of this project.
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
 
-// package frc.robot.commands.Scoring;
+package frc.robot.commands.Scoring;
 
-// import java.util.List;
+import java.util.List;
 
-// import edu.wpi.first.math.controller.PIDController;
-// import edu.wpi.first.math.controller.ProfiledPIDController;
-// import edu.wpi.first.math.geometry.Pose2d;
-// import edu.wpi.first.math.geometry.Translation2d;
-// import edu.wpi.first.math.trajectory.Trajectory;
-// import edu.wpi.first.math.trajectory.TrajectoryConfig;
-// import edu.wpi.first.math.trajectory.TrajectoryGenerator;
-// import edu.wpi.first.wpilibj2.command.InstantCommand;
-// import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-// import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
-// import frc.robot.Constants.AutoConstants;
-// import frc.robot.Constants.Swerve;
-// import frc.robot.commands.SwerveCommands.AlignScoringCommand;
-// import frc.robot.subsystems.SwerveSubsystem;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrajectoryConfig;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
+import frc.robot.Constants.AutoConstants;
+import frc.robot.Constants.Swerve;
+import frc.robot.commands.SwerveCommands.AlignScoringCommand;
+import frc.robot.interfaces.LimelightInterface;
+import frc.robot.interfaces.LimelightInterface.Limelight;
+import frc.robot.subsystems.SwerveSubsystem;
 
-// // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
-// // information, see:
-// // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-// public class TrajectoryToGoal extends SequentialCommandGroup {
-//   public TrajectoryToGoal(SwerveSubsystem swerve) {
-//     TrajectoryConfig config = new TrajectoryConfig(
-//             AutoConstants.kMaxSpeedMetersPerSecond,
-//             AutoConstants.kMaxAccelerationMetersPerSecondSquared)
-//     .setKinematics(Swerve.swerveKinematics);
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class TrajectoryToGoal extends SequentialCommandGroup {
+  TrajectoryConfig config = new TrajectoryConfig(
+    AutoConstants.kMaxSpeedMetersPerSecond,
+    AutoConstants.kMaxAccelerationMetersPerSecondSquared)
+.setKinematics(Swerve.swerveKinematics);
 
-//     config.setStartVelocity(0);
-//     config.setEndVelocity(0);
-//     config.setReversed(false); //TO-DO: check if correct orientation
+  private SwerveSubsystem swerve;
 
-//     /*Generate the trajectory based on waypoints */
+  public TrajectoryToGoal(SwerveSubsystem swerve) {
+    this.swerve = swerve;
+    config.setStartVelocity(0);
+    config.setEndVelocity(0);
+    config.setReversed(false); //TO-DO: check if correct orientation
+
+    /*Generate the trajectory based on waypoints */
     
 
-//     var thetaController =
-//         new ProfiledPIDController(
-//             AutoConstants.kPThetaController, 0, 0.001, AutoConstants.kThetaControllerConstraints);
-//     thetaController.enableContinuousInput(-Math.PI, Math.PI);
+    var thetaController =
+        new ProfiledPIDController(
+            AutoConstants.kPThetaController, 0, 0.001, AutoConstants.kThetaControllerConstraints);
+    thetaController.enableContinuousInput(-Math.PI, Math.PI);
+            
 
-        
-//     // AlignScoringCommand swerveControllerCommand =
-//     //     new AlignScoringCommand(
-//     //         trajectory,
-//     //         swerve::getPose,
-//     //         Swerve.swerveKinematics,
-//     //         new PIDController(AutoConstants.kPXController, 0, 0),
-//     //         new PIDController(AutoConstants.kPYController, 0, 0),
-//     //         thetaController,
-//     //         swerve::setModuleStates,
-//     //         swerve);
-
-    
-//     addCommands(
-//       new InstantCommand(() -> swerve.resetOdometry(trajectory.getInitialPose())),
-//       swerveControllerCommand
-//     );
-//   }
+    addCommands(
+      new SwerveControllerCommand(
+          generateTrajectory(),
+          swerve::getPose,
+          Swerve.swerveKinematics,
+          new PIDController(AutoConstants.kPXController, 0, 0),
+          new PIDController(AutoConstants.kPYController, 0, 0),
+          thetaController,
+          swerve::setModuleStates,
+          swerve)
+    );
+  }
   
+  public Trajectory generateTrajectory(){
+    Pose2d center = new Pose2d(0, 0, null);
+    Pose2d left = new Pose2d(0, -0.5, null);
+    Pose2d right = new Pose2d(0, 0.5, null);
 
-  
-// }
+    Limelight limelight = swerve.getCorrectLimelight();
+    Pose3d currentRobotPose = LimelightInterface.getInstance().getRobotPose(limelight);
+    double robotX = 0;
+    double robotY = 0;
+    /* rotation */
+    if(limelight == Limelight.LEFT_LIMELIGHT){
+      robotX = -currentRobotPose.getY();
+      robotY = currentRobotPose.getX();
+    }
+    else{
+      robotX = currentRobotPose.getY();
+      robotY = -currentRobotPose.getX();
+    }
+
+    return TrajectoryGenerator.generateTrajectory(
+                //current bot pose
+                new Pose2d(robotX, robotY, new Rotation2d(currentRobotPose.getRotation().getX())),
+                //intersection point
+                List.of(new Translation2d(1, 1)),
+                //target point
+                new Pose2d(2, 0, new Rotation2d(0)),
+                config);
+  }
+
+}
