186b83062d13b677fd9458047c74acea681e8f8a
Aalind Tyagi <42896179+Squid5678@users.noreply.github.com>
2023-01-28 16:59:05 -0500

    B.1.5.2.1.SI - Fixed Color Sensor Stuff, Encoder Testing

--- src/main/java/frc/robot/interfaces/ColorSensorInterface.java
+++ src/main/java/frc/robot/interfaces/ColorSensorInterface.java
@@ -4,8 +4,10 @@
 
 package frc.robot.interfaces;
 
+import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import frc.robot.Constants.GamePieceColorConstants;
 import frc.robot.MKILib.MKIPicoColorSensor;
 import frc.robot.MKILib.MKIPicoColorSensor.RawColor;
 
@@ -13,64 +15,99 @@ public class ColorSensorInterface extends SubsystemBase {
 
   private static ColorSensorInterface color;
 
-  private static MKIPicoColorSensor pico;
+  private MKIPicoColorSensor pico;
+
+  private RawColor rColor0 = new RawColor();
+
+  double red = 0;
+  double blue = 0;
+  double green = 0;
+
+  public enum GamePiece{
+    YELLOW_CONE,
+    PURPLE_CUBE,
+    UNKNOWN
+  }
   
   public static synchronized ColorSensorInterface getInstance() {
     if (color == null) {
-      color = new ColorSensorInterface();
+      color = new ColorSensorInterface(new MKIPicoColorSensor());
     }
     return color;
   }
   /** Creates a new ColorSensorInterface. */
-  public ColorSensorInterface() {}
+  public ColorSensorInterface(MKIPicoColorSensor p) {
+    this.pico = p;
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+
+    rColor0  =  pico.getRawColor0();
+    red = GetRed();
+    blue = GetBlue();
+    green = GetGreen();  
+
   }
 
   //Ignoring red comparison 
-  public boolean isCube(int r, int g, int b){  //purple cube is (151,23,160), 
+  public boolean isCube(){  //purple cube is (151,23,160), 
  
-   if(Math.abs(g - Constants.GamePieceColorConstants.CUBEGREEN) <= 25 && Math.abs(b - Constants.GamePieceColorConstants.CUBEBLUE) <= 25){
-    return true;
-   }
-   else{
-    return false;
-   }
+    return (blue/green < Constants.GamePieceColorConstants.UNKNOWN_THRESHOLD - Constants.GamePieceColorConstants.Offset);
   }
 
-  public boolean isCone(int r, int g, int b){ // yellow cone is (177,166,12)
-    if(Math.abs(g - Constants.GamePieceColorConstants.CONEGREEN) <= 25 && Math.abs(b - Constants.GamePieceColorConstants.CONEBLUE) <= 25){
-      return true;
-     }
-     else{
-      return false;
-     }
+  public boolean isCone(){ // yellow cone is (177,166,12)
+    return (blue/green > Constants.GamePieceColorConstants.UNKNOWN_THRESHOLD + Constants.GamePieceColorConstants.Offset);
   }
 
-  public int getGamePiece(){
-    RawColor rcolor0  =  pico.getRawColor0();
-    RawColor rcolor1 = pico.getRawColor1();
+  public String RGBValues(){
+
+    return "Red: " +GetRed()+" Blue: "+ GetBlue()+" Green: "+GetGreen();
+
+  }
+
+  public double GetRed(){
+    return rColor0.red;
+  }
+  public double GetBlue(){
+    return rColor0.blue;
+  }
+  public double GetGreen(){
+    return rColor0.green;
+  }
+
+  public GamePiece getGamePiece(){
+
+    //all things realated to color sensor 1 commented since testing was done with only one color sensor
+
+    //RawColor rcolor1 = pico.getRawColor1();
  
-    int red = (rcolor0.red + rcolor1.red)/2;
-    int blue = (rcolor0.blue + rcolor1.blue)/2;
-    int green = (rcolor0.green + rcolor1.green)/2;
+    // int red = (rcolor0.red + rcolor1.red)/2;
+    // int blue = (rcolor0.blue + rcolor1.blue)/2;
+    // int green = (rcolor0.green + rcolor1.green)/2;  
+
 
     // implement ir break sensor if logic later
 
-    if(!BeamBreakInterface.getInstance().isBroken()){
-      return 0;
-    }
-    else if(isCube(red, green, blue)) {
+    // if(!BeamBreakInterface.getInstance().isBroken()){
+    //   return 0;
+    // }
+    /*else*/
 
-      return 1;
+
+    if(isCube()) {
+
+      return GamePiece.PURPLE_CUBE;
 
     }
-    else if (isCone(red, green, blue)) {
-      return -1;
+    else if (isCone()) {
+      return GamePiece.YELLOW_CONE;
     }
-   return 0;
-  }
+   else {
+    
+      return GamePiece.UNKNOWN;
+   }
+ }
 }
 
