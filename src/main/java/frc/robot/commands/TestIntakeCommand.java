33f507881e4c3f193939ec7150fb60b6004ea5f9
Aalind Tyagi <42896179+Squid5678@users.noreply.github.com>
2023-02-05 20:24:21 -0500

    update, some file didn't save before

--- src/main/java/frc/robot/commands/TestIntakeCommand.java
+++ src/main/java/frc/robot/commands/TestIntakeCommand.java
@@ -25,13 +25,13 @@ public class TestIntakeCommand extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    intake.intake(stpt);
+    intake.runIntake(stpt);
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    intake.intake(0);
+    intake.runIntake(0);
   }
 
   // Returns true when the command should end.
