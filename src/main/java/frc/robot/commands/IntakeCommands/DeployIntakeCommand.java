36474bd04527a0fc0a91a2a476a35e6295164f32
sammulvey747 <sam.mulvey747@gmail.com>
2023-02-09 12:08:20 -0500

    Fix for deploy intake commnad

--- src/main/java/frc/robot/commands/IntakeCommands/DeployIntakeCommand.java
+++ src/main/java/frc/robot/commands/IntakeCommands/DeployIntakeCommand.java
@@ -18,15 +18,16 @@ public class DeployIntakeCommand extends CommandBase {
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
-
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
+  public void initialize() {
     IntakePositions currentIntakePosition = stateHandler.getCurrentIntakePosition();
     if(currentIntakePosition == IntakePositions.STOW){
       stateHandler.setDesiredIntakePosition(IntakePositions.REVERSE_HANDOFF_2);
     }
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
   }
 
   // Called once the command ends or is interrupted.
@@ -36,6 +37,6 @@ public class DeployIntakeCommand extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return true;
   }
 }
