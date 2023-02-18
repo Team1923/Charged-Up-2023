6354e187e68529ca6751899a4236f8bab816418b
Aalind Tyagi <42896179+Squid5678@users.noreply.github.com>
2023-02-18 18:24:05 -0500

    Merge branch 'beta-environment' of https://github.com/Team1923/Charged-Up-2023 into beta-environment

---
+++ src/main/java/frc/robot/interfaces/ArmLedInterface.java
@@ -0,0 +1,121 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.interfaces;
+
+import java.util.ArrayList;
+import edu.wpi.first.hal.simulation.AddressableLEDDataJNI;
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ArmLedInterface extends SubsystemBase {
+
+  private static ArmLedInterface ledInterface;
+  private int currentRed = 0; 
+  private boolean getBrighter = true;
+
+  private static AddressableLED led;
+  private static AddressableLEDBuffer ledbuffer = new AddressableLEDBuffer(100);
+
+  private static int rainbowFirstPixelHue = 0;
+
+
+  public static synchronized ArmLedInterface getInstance() {
+    if (ledInterface == null) {
+      ledInterface = new ArmLedInterface();
+    }
+    return ledInterface;
+  }
+
+  /** Creates a new LedInterface. */
+  public ArmLedInterface() {
+    led =  new AddressableLED(0);
+    led.setLength(ledbuffer.getLength());
+    led.start();
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  public void setGreen() {
+    for(int i = 0; i < ledbuffer.getLength(); i ++){
+      ledbuffer.setRGB(i,0,255,0);
+    }
+    led.setData(ledbuffer);
+  }
+
+  public void setRed(){
+    for(int i = 0; i < ledbuffer.getLength(); i ++){
+      ledbuffer.setRGB(i, 255, 0, 0);
+    }
+    led.setData(ledbuffer);
+  }
+
+  public void setYellow(){
+    for(int i = 0; i < ledbuffer.getLength(); i++){
+      ledbuffer.setRGB(i, 255, 255, 0);
+    }
+    led.setData(ledbuffer);
+  }
+
+  public void setPurple(){
+    for(int i = 0; i < ledbuffer.getLength(); i++) {
+      ledbuffer.setRGB(i, 119, 0, 200);
+    }
+    led.setData(ledbuffer);
+  }
+
+  public void setBlank() {
+    for(int i = 0; i < ledbuffer.getLength(); i++) {
+      ledbuffer.setRGB(i, 0, 0, 0);
+    }
+    led.setData(ledbuffer);
+  }
+
+  public void setIndividualLed(ArrayList<Color> a) {
+    for(int i = 0; i < ledbuffer.getLength(); i++) {
+      ledbuffer.setLED(i, a.get(i));
+    }
+    led.setData(ledbuffer);
+  }
+
+  public void rainbow(){//Works
+    for(var i = 0; i < ledbuffer.getLength(); i++){
+      final var hue = (rainbowFirstPixelHue + (i * 180/ledbuffer.getLength())) % 180;
+
+      ledbuffer.setHSV(i, hue, 255, 128);
+    }
+    rainbowFirstPixelHue += 3;
+    rainbowFirstPixelHue %= 180;
+
+    led.setData(ledbuffer);
+  }
+
+  public void redOsciliating(){
+    if(getBrighter){
+      currentRed +=15;
+    }
+    else{
+      currentRed -=15;
+    }
+
+    for(int i = 0; i < ledbuffer.getLength(); i ++){
+      ledbuffer.setRGB(i, currentRed, 0, 0);
+      if(currentRed >= 255){
+        currentRed = 255;
+        getBrighter = false;
+      }
+      else if(currentRed <= 0){
+        currentRed = 0; 
+        getBrighter = true;
+      }
+    }
+    led.setData(ledbuffer);
+  }
+
+}
