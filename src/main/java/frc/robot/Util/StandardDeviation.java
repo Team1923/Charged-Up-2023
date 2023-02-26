6243a4d8dd80aed7c2c1b8a3f875bae91f7e8870
s1oly <90860963+s1oly@users.noreply.github.com>
2023-02-26 14:28:23 -0500

    StandardDeviation Work

--- src/main/java/frc/robot/Util/StandardDeviation.java
+++ src/main/java/frc/robot/Util/StandardDeviation.java
@@ -3,19 +3,65 @@ package frc.robot.Util;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+
 import frc.robot.interfaces.BetterLimelightInterface;
+import frc.robot.interfaces.BetterLimelightInterface.SpecificLimelight;
 
 public class StandardDeviation {
 
     BetterLimelightInterface limelightInterface = BetterLimelightInterface.getInstance();
 
-    ArrayList<Double> xstd = new ArrayList<>();
-    ArrayList<Double> ystd = new ArrayList<>();
-    ArrayList<Double> thetastd = new ArrayList<>();
+    ArrayList<Double> xdeviation = new ArrayList<>();
+    ArrayList<Double> ydeviation = new ArrayList<>();
+    ArrayList<Double> yawdeviation = new ArrayList<>();
 
-    HashMap<Integer, Double> xdistance = new HashMap<>();
-    HashMap<Integer, Double> ydistance = new HashMap<>();
-    HashMap<Integer, Double> thetadistance = new HashMap<>();
+    HashMap<Double, Double> xstd = new HashMap<>();
+    HashMap<Double, Double> ystd = new HashMap<>();
+    HashMap<Double, Double> yawstd = new HashMap<>();
+
+    public void xSample(SpecificLimelight limelight){
+        while(limelightInterface.hasScoringTarget()){
+            xdeviation.add(limelightInterface.getBotPose(limelight)[0]);
+        }
+    }
+    public void ySample(SpecificLimelight limelight){
+        while(limelightInterface.hasScoringTarget()){
+            xdeviation.add(limelightInterface.getBotPose(limelight)[1]);
+        }
+    }
+    public void yawSample(SpecificLimelight limelight){
+        while(limelightInterface.hasScoringTarget()){
+            xdeviation.add(limelightInterface.getBotPose(limelight)[5]);
+        }
+    }
+    public void xMap(SpecificLimelight limelight){
+      while(limelightInterface.hasScoringTarget()){
+        xstd.put(limelightInterface.getBotPose(limelight)[2],calculateStandardDeviation(xdeviation));
+      }
+    }
+    public void yMap(SpecificLimelight limelight){
+        while(limelightInterface.hasScoringTarget()){
+          ystd.put(limelightInterface.getBotPose(limelight)[2],calculateStandardDeviation(ydeviation));
+        }
+      }
+      public void yawMap(SpecificLimelight limelight){
+        while(limelightInterface.hasScoringTarget()){
+          yawstd.put(limelightInterface.getBotPose(limelight)[2],calculateStandardDeviation(yawdeviation));
+        }
+      }
+
+     public double  calculateStandardDeviation(ArrayList<Double> Sample){
+        double StandardDeviation =0 , sum = 0;
+        for(double n: Sample){
+            sum += n; 
+        }
+        for(double n : Sample){
+                StandardDeviation += Math.pow(n - (sum/Sample.size()), 2);
+            }
+
+            return Math.sqrt(StandardDeviation/Sample.size());
+     }
     
+     
     
 }
