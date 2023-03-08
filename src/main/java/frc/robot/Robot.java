6459c26db0f140a257cb18fc7c818f1c798dd72a
Aalind Tyagi <42896179+Squid5678@users.noreply.github.com>
2023-03-08 16:46:33 -0500

    pbot instant command reset states

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -14,6 +14,7 @@ import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.commands.EmergencyCommands.CheckArmAngles;
 import frc.robot.commands.EmergencyCommands.EStopArmCommand;
 import frc.robot.commands.EmergencyCommands.EStopIntakeCommand;
@@ -119,6 +120,8 @@ public class Robot extends TimedRobot {
     SmartDashboard.putBoolean("INTAKE GOOD TO GO", intakeGood);
 
     SmartDashboard.putBoolean("ARM GOOD TO GO", armGood);
+
+    SmartDashboard.putData("RESET STATES", new InstantCommand(() -> stateHandler.resetStates()));
 
   }
 
