f69726785c80fef6f403b1f85d4e1445aedcfb33
Aalind Tyagi <42896179+Squid5678@users.noreply.github.com>
2023-11-02 19:41:23 -0500

    11.2 right after lab

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -85,7 +85,7 @@ public final class Constants {
         COTSFalconSwerveConstants.SDSMK4i(COTSFalconSwerveConstants.driveGearRatios.SDSMK4i_L3);
 
     /* Drivetrain Constants */
-    public static final double trackWidth = Units.inchesToMeters(18.75); // TODO: This must be tuned to specific robot
+    public static final double trackWidth = Units.inchesToMeters(18.75); // TODO: his must be tuned to specific robot
     public static final double wheelBase = Units.inchesToMeters(18.75); // TODO: This must be tuned to specific robot
     public static final double wheelCircumference = chosenModule.wheelCircumference;
 
